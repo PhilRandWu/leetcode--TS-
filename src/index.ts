431a598153191a83e1279e728752a600d06c359a
PhilRandWu <3378814052@qq.com>
2022-06-12 20:00:53 +0800

    利用数组 reverse 函数

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@
  * @Author: PhilRandWu
  * @Github: https://github/PhilRandWu
  * @Date: 2022-06-12 19:16:01
- * @LastEditTime: 2022-06-12 19:47:37
+ * @LastEditTime: 2022-06-12 19:57:28
  * @LastEditors: PhilRandWu
  */
-// import { twoSum } from "./1.两数之和";
 
-// console.log(twoSum([3,2,4],6))
+import { isPalindrome } from "./9.回文数";
+
+console.log(isPalindrome(-121))
