48fe798b126ab8158ac0309ef3176570fa8a32a0
PhilRandWu <3378814052@qq.com>
2022-06-12 20:33:45 +0800

    最大字符串前缀(利用 reduce 巧解)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
-import { romanToInt } from "./13.罗马数字转整数";
+import { longestCommonPrefix } from './14.最长公共前缀'
+console.log(longestCommonPrefix(["flower", "flow", "flight"]))
 
-/*
- * @Description: 
- * @Author: PhilRandWu
- * @Github: https://github/PhilRandWu
- * @Date: 2022-06-12 19:16:01
- * @LastEditTime: 2022-06-12 20:17:50
- * @LastEditors: PhilRandWu
- */
-console.log(romanToInt("MCMXCIV"));
