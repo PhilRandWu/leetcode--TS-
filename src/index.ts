af63d6ee7dd41f481b69c7d203aec88b091930c2
PhilRandWu <3378814052@qq.com>
2022-06-17 16:42:51 +0800

    链表

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
-import { climbStairs } from "./70.爬楼梯";
+import { deleteDuplicates } from "./83.删除排序链表中的重复元素";
 
 /*
  * @Description: 
  * @Author: PhilRandWu
  * @Github: https://github/PhilRandWu
- * @Date: 2022-06-17 15:54:24
- * @LastEditTime: 2022-06-17 15:59:35
+ * @Date: 2022-06-17 16:26:57
+ * @LastEditTime: 2022-06-17 16:26:57
  * @LastEditors: PhilRandWu
  */
-console.log(climbStairs(45));
-// 1836311903
+console.log(deleteDuplicates([1,1,2]))
